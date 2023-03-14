74cc259054d0af77ccfe980a35f6ebac7757a926
mnbvmnbv2 <ferdinand@ytteborg.com>
2023-03-14 05:57:31 +0100

    rename loader.py to problem.py

--- python/problem.py
+++ python/problem.py
@@ -1,26 +1,29 @@
 import json
 import numpy as np
+from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 
 
-def load_json(path):
-    with open(path, "r", encoding="UTF-8") as file:
-        return json.load(file)
-
-
 class Problem:
+    """A problem class that loads from json file,
+    handles visualization and evaluation of solutions."""
+
     def __init__(self, file: str) -> None:
         self.file = file
-        self.data = load_json(file)
+        with open(file, "r", encoding="UTF-8") as file:
+            self.data = json.load(file)
+
         self.instance_name = self.data["instance_name"]
         self.nbr_nurses = self.data["nbr_nurses"]
         self.capacity_nurse = self.data["capacity_nurse"]
         self.depot = self.data["depot"]
         self.patients = self.data["patients"]
+        # assumes id 1-len(patients) are used
         self.nbr_patients = len(self.patients)
         self.travel_times = np.asarray(self.data["travel_times"])
 
-    def visualize(self):
+    def visualize_problem(self) -> None:
+        """Visualize the problem instance."""
         # plot depot
         plt.scatter(self.depot["x_coord"], self.depot["y_coord"], c="r")
         # plot patients
@@ -30,6 +33,31 @@ class Problem:
                 self.patients[idx]["x_coord"], self.patients[idx]["y_coord"], c="b"
             )
         plt.show()
+
+    def evaluate(self, solution: NDArray) -> float:
+        """Evalauates a solution.
+
+        Args:
+            solution (NDArray): A potential solution.
+            Solution is on format one row per nurse, one hot encoded for each patient visisted.
+
+        Returns:
+            float: fitness of the solution.
+        """
+        # TODO
+        return 0.0
+
+    def visualize_solution(self, solution: NDArray) -> None:
+        """Visualize a solution."""
+        # plot depot
+        plt.scatter(self.depot["x_coord"], self.depot["y_coord"], c="r")
+        # plot patients
+        for idx in range(1, self.nbr_patients):
+            idx = str(idx)
+            plt.scatter(
+                self.patients[idx]["x_coord"], self.patients[idx]["y_coord"], c="b"
+            )
+        # TODO plot solution routes
 
 
 if __name__ == "__main__":
@@ -42,7 +70,7 @@ if __name__ == "__main__":
     print(problem.patients)
     print(problem.travel_times)
     print(problem.nbr_patients)
-    problem.visualize()
+    problem.visualize_problem()
 
     # Run the GA
     # ga = GA(problem, data)
