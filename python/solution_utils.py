9f30a74363872af3992ca53b3bb4898790891860
mnbvmnbv2 <ferdinand@ytteborg.com>
2023-03-18 23:31:00 +0100

    add clustered and greedy populations

--- python/solution_utils.py
+++ python/solution_utils.py
@@ -3,6 +3,7 @@ import itertools
 import numpy as np
 from numpy.typing import NDArray
 from numba import njit
+from sklearn.cluster import KMeans
 
 
 def solution_to_numpy(solution: list) -> NDArray:
@@ -114,3 +115,175 @@ def generate_random_population(size: int, n_nurses: int, n_patients: int) -> NDA
     genomes = genome_.reshape((size, n_nurses, n_patients))
 
     return genomes
+
+
+def generate_cluster_genome(
+    n_nurses: int, n_patients: int, coordinates: np.ndarray
+) -> np.ndarray:
+    """Generate a genome based on clustering.
+
+    Args:
+        n_nurses (int): determine size of genome
+        n_patients (int): determine size of genome
+        coordinates (np.ndarray): coordinates of patients
+
+    Returns:
+        np.ndarray: genome
+    """
+    genome = np.zeros((n_nurses, n_patients), dtype=np.int16)
+    # randomize how many clusters
+    n_clusters = np.random.randint(n_nurses // 1.5, n_nurses + 1)
+    # generate clusters with k nearest neighbors
+    neigh = KMeans(n_clusters=n_clusters, random_state=0).fit(coordinates)
+    # get cluster labels
+    cluster_labels = neigh.labels_
+    # insert patients into genome
+    for nurse_idx in range(n_nurses):
+        # get patients in cluster
+        patients_in_cluster = np.where(cluster_labels == nurse_idx)[0]
+        # shuffle patients
+        np.random.shuffle(patients_in_cluster)
+        # get patients to insert
+        cluster_length = patients_in_cluster.shape[0]
+        # insert patients
+        genome[nurse_idx, :cluster_length] = patients_in_cluster
+    return genome
+
+
+def generate_cluster_population(
+    size: int, n_nurses: int, n_patients: int, travel_times: np.ndarray
+) -> NDArray:
+    """Generate a population based on clustering.
+
+    Args:
+        size (int): number of individuals in population
+        n_nurses (int): determine size of genome
+        n_patients (int): determine size of genome
+        travel_times (np.ndarray): travel times between patients
+
+    Returns:
+        NDArray: genomes
+    """
+    pass
+
+
+# TODO fix
+def generate_greedy_genome(
+    n_nurses: int, n_patients: int, travel_times: np.ndarray
+) -> np.ndarray:
+    genome = np.zeros((n_nurses, n_patients), dtype=np.int16)
+    first_patients = np.random.choice(
+        np.arange(1, n_patients + 1), size=n_nurses, replace=False
+    )
+    for nurse_idx in range(n_nurses):
+        genome[nurse_idx, 0] = first_patients[nurse_idx]
+
+    # fill in used patients
+    used_patients = np.zeros(n_patients, dtype=np.int16)
+    used_patients[: first_patients.shape[0]] = first_patients
+    used_patients_num = first_patients.shape[0]
+
+    remaining_patients = np.setdiff1d(np.arange(1, n_patients + 1), first_patients)
+
+    # shuffle remaining patients
+    np.random.shuffle(remaining_patients)
+    for patient in remaining_patients:
+        # find shortest travel times
+        shortest_travel_time_idx = np.argsort(travel_times)
+        # pick one of the shortest travel times
+        idx_pick = np.random.randint(0, 5)
+        shortest_travel_time_idx = shortest_travel_time_idx[idx_pick]
+        if shortest_travel_time_idx in used_patients:
+            # insert as next in path (get spot to put after)
+            spot = np.where(genome == shortest_travel_time_idx)
+            # make space
+            genome[spot[0][0], spot[0][1] + 2 : spot[0][1] + 11] = genome[
+                spot[0][0], spot[0][1] + 1 : spot[0][1] + 10
+            ]
+            # insert
+            genome[spot[0][0], spot[0][1] + 1] = patient
+        else:
+            # insert randomly
+            nurse_idx = np.random.randint(0, n_nurses)
+            spot = np.where(genome[nurse_idx] == 0)
+            genome[nurse_idx, spot[0][0]] = patient
+        used_patients[used_patients_num] = patient
+        used_patients_num += 1
+    return genome
+
+
+# TODO fix
+def generate_greedy_population(
+    size: int, n_nurses: int, n_patients: int, travel_times: np.ndarray
+) -> NDArray:
+    """Generate a population based on greedy search."""
+    population = np.zeros((size, n_nurses, n_patients), dtype=np.int16)
+    # assign one random patient to each nurse
+    for genome in population:
+        genome_ = generate_greedy_genome(
+            n_nurses=n_nurses, n_patients=n_patients, travel_times=travel_times
+        )
+        genome[:, :] = genome_[:, :]
+
+    # assign remaining patients to nurse with shortest travel time
+    return population
+
+
+def timing():
+    from problem import Problem
+
+    problem = Problem("data/train_0.json")
+    travel_times = problem.travel_times
+
+    pop1 = generate_random_population(size=100, n_nurses=25, n_patients=100)
+    pop2 = generate_cluster_population(
+        size=100, n_nurses=25, n_patients=100, travel_times=travel_times
+    )
+
+
+def testing():
+    from problem import Problem
+    from ga import evaluate_population, evaluate
+    from population_manage import sort_population
+
+    problem = Problem("data/train_0.json")
+    travel_times = problem.travel_times
+    capacity_nurse = problem.capacity_nurse
+    patients = problem.numpy_patients
+    coordinates = patients[:, :2]
+
+    genome = generate_cluster_genome(
+        n_nurses=25, n_patients=100, coordinates=coordinates
+    )
+
+    # population = generate_greedy_population(
+    #     size=100, n_nurses=25, n_patients=100, travel_times=travel_times
+    # )
+    # fitness, valids = evaluate_population(population)
+    # population, fitness, valids = sort_population(
+    #     genomes=population,
+    #     fitness=fitness,
+    #     valids=valids,
+    # )
+    fitness, valid = evaluate(
+        genome,
+        travel_times,
+        capacity_nurse,
+        patients,
+    )
+    print(fitness)
+    problem.visualize_solution(genome)
+
+
+if __name__ == "__main__":
+    import cProfile
+    import pstats
+
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    testing()
+
+    profiler.disable()
+    stats = pstats.Stats(profiler)
+    stats.dump_stats("profile/pop-gen.prof")
